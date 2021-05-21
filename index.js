525af148903375ff3f0f6c103dc9bf4041d19341
proffillipesilva <81185103+proffillipesilva@users.noreply.github.com>
2021-05-21 17:15:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const app = express();
 app.use(cors());
 app.use(morgan('combined'));
 
-app.get('/hello', (req,res) => { res.end('Olá Aluninhos da Fiec - VIA GIT - 3mod 2021'); });
+app.get('/hello', (req,res) => { res.end(`
+<html>
+<body>
+<h1> Aulinha Feliz de Cloud da Fiec </h1>
+<img src="https://proffillipesilva.s3.us-east-2.amazonaws.com/foto_da_turma.png" />
+</body>
+</html>
+`); });
 
 app.listen(3000, () => console.log('Servidor ligado'));
