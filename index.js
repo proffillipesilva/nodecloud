4824fed132e59bb236736dde20dfa59c4ab2f4a0
proffillipesilva <81185103+proffillipesilva@users.noreply.github.com>
2021-06-11 17:01:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,15 @@ con.connect(function(err) {
 app.use(cors());
 app.use(morgan('combined'));
 
+app.get('/', (req, res) => { res.end(`
+<html>
+<body>
+<h1> Aulinha Feliz de Cloud da Fiec </h1>
+<img src="https://proffillipesilva.s3.us-east-2.amazonaws.com/foto_da_turma.png" />
+</body>
+</html>
+`); });
+
 app.get('/hello', (req, res) => { res.end(`
 <html>
 <body>
